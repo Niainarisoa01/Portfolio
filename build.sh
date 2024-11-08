b7b7763a288d528f069947c42bff5d8cc0204c44
Niainarisoa01 <niainarisoa.mail@gmail.com>
2024-11-08 10:07:49 +0300

    Make build.sh executable

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+
+# Mise à jour de pip
 python -m pip install --upgrade pip
+
+# Installation des dépendances Python
 pip install -r requirements.txt
+
+# Installation explicite de gunicorn
+pip install gunicorn
+
+# Installation des dépendances Node.js et build de Tailwind
 npm install
-npm run build 
+npm run build
+
+# Ajout du chemin Python au PATH
+export PATH=$PATH:/usr/local/python3/bin 
